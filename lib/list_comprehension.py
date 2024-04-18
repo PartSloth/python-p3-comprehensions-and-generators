ff584acbc45f5fb67d6dccbc60b2e59a0b78d2f3
Louiza Mak <louiza.mak@gmail.com>
2024-04-18 12:13:37 -0500

    complete lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    divided_list = [num % 2 for num in num_list]
+    even_list = [num for num in num_list if num % 2 == 0]
+    if(divided_list.count(not 0) == len(divided_list)): return []
+    else: return even_list
 
 def make_exclamation(sentence_list):
-    pass
+    if(len(sentence_list) == 0): return []
+    else: 
+        sentence_list = [sentence + '!' for sentence in sentence_list]
+        return sentence_list
